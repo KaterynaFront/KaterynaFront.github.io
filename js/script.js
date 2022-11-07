7d81690b55aef1d08cd8635cfd2c64cdd889e22d
Kate <example@gmail.com>
2022-11-07 16:25:45 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function($) {
+    $("#scroll-top > button").on("click", function(e) {
+      var body = $("html, body");
+      body.stop().animate({ scrollTop: 0 }, 500, "swing");
+    });
+  });
+  
