43c26f50cad10910bbf65b60f6f19a0e541531b3
Kate <example@gmail.com>
2023-01-11 16:25:23 +0200

    Add choose material

--- js/script.js
+++ js/script.js
@@ -43,6 +43,51 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+window.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.querySelectorAll(".material-item"),
+    tabsContent = document.querySelectorAll(".content-materials"),
+    tabsParent = document.querySelector(".materials-items"),
+    tabsShowImg = document.querySelectorAll(".choose-materials-art");
+
+  function hideTabContent() {
+    tabsContent.forEach((item) => {
+      item.classList.add("hide");
+      item.classList.remove("show", "fade");
+    });
+    tabsShowImg.forEach((item) => {
+      item.classList.add("hide");
+      item.classList.remove("show", "fade");
+    });
+
+    tabs.forEach((item) => {
+      item.classList.remove("materials-active");
+    });
+  }
+
+  function showTabContent(i = 0) {
+    tabsContent[i].classList.add("show", "fade");
+    tabsContent[i].classList.remove("hide");
+    tabsShowImg[i].classList.add("show", "fade");
+    tabsShowImg[i].classList.remove("hide");
+    tabs[i].classList.add("materials-active");
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  tabsParent.addEventListener("click", function (event) {
+    const target = event.target;
+    if (target && target.classList.contains("material-item")) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+});
+
 jQuery(document).ready(function ($) {
   $("#scroll-top > button").on("click", function (e) {
     var body = $("html, body");
